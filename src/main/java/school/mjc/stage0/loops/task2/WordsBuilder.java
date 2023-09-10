68dbee94fd0cc7370464ddabff22157e7db84d03
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-10 23:34:21 +0400

    uhh (changedFour)

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,14 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        StringBuilder stringBuilder = new StringBuilder();
 
         int i = 0;
-        while (i < chars.length - 1) {
-            stringBuilder.append(chars[i]);
+        while (i < chars.length) {
+            System.out.print(chars[i]);
             i++;
         }
-
-        System.out.println(stringBuilder);
     }
 }
